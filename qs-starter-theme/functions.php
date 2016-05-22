ee083fb318dd45acaa6fb5605f1b195ad47211a7
nivnoiman <niv945@gmail.com>
2016-05-22 12:15:10 +0300

    Update Starter THEME
    Update Starter THEME

--- qs-starter-theme/functions.php
+++ qs-starter-theme/functions.php
@@ -1,25 +1,76 @@
 <?php
-define("THEME", get_template_directory_uri());
-//if wpml
-// define("LANG",ICL_LANGUAGE_CODE);
-define("LANG", "he");
-if(is_rtl()) {
-    define("FOUNDATION", THEME.'/foundation-6.2.1-rtl');
-} else {
-    define("FOUNDATION", THEME.'/foundation-6.2.1-ltr');
+/*****************************************
+**  Define
+*****************************************/
+    if( !defined('THEME') )
+        define("THEME", get_template_directory_uri());
+    //if wpml
+    // define("LANG",ICL_LANGUAGE_CODE);
+    define("LANG", "he");
+    if(is_rtl())
+        define("FOUNDATION", THEME.'/foundation-6.2.1-rtl');
+    else
+        define("FOUNDATION", THEME.'/foundation-6.2.1-ltr');
+
+    if( !defined('TEMPLATEPATH') )
+    	define( 'TEMPLATEPATH', get_template_directory() );
+
+/*****************************************
+**  Languages
+*****************************************/
+// Localisation Support
+add_action('after_setup_theme', 'qstheme_textdomain');
+function qstheme_textdomain(){
+    load_theme_textdomain('qstheme', THEME . '/languages');
 }
 
-require_once( dirname( __FILE__ ) . '/admin/options.php' );
-require_once( dirname( __FILE__ ) . '/admin/types.php' );
 
-/***************************************
-		Plugins
-***************************************/
+/*****************************************
+**  Hooks && Filters
+*****************************************/
+    require_once('functions/hooks.php');
 
+/*****************************************
+**  ACF Framework
+*****************************************/
+    require_once( TEMPLATEPATH . '/admin/options.php' );
+
+/*****************************************
+**  Includes
+*****************************************/
+
+    include TEMPLATEPATH . '/functions/core-functions.php';
+
+    include TEMPLATEPATH . '/functions/functions.php';
+
+    require_once( TEMPLATEPATH . '/admin/types.php' );
+
+    require_once( TEMPLATEPATH . '/admin/ajax_function.php');
+
+/*****************************************
+**  Menu
+*****************************************/
+    require_once('functions/menus.php');
+
+/*****************************************
+**  Widgets INIT
+*****************************************/
+    require_once('functions/widgets.php');
+
+/*****************************************
+**  Plugin
+*****************************************/
 //Page children menu
-get_template_part("functions/get_page_children");
+get_template_part("functions/plugins/get_page_children");
 // Call to function with:  build_menu_list($post, 'page',true);
 
+//Get Image Plugin
+get_template_part("functions/plugins/get_image");
+// Call to function with:  get_image( array() ) // for more info https://github.com/nivnoiman/get_image
+
+/*****************************************
+**  Global
+*****************************************/
 /*------------------------------------*\
 	Theme Support
 \*------------------------------------*/
@@ -28,8 +79,7 @@ if (!isset($content_width)) {
     $content_width = 1024;
 }
 
-if (function_exists('add_theme_support'))
-{
+if (function_exists('add_theme_support')){
     // Add Menu Support
     add_theme_support('menus');
 
@@ -41,317 +91,4 @@ if (function_exists('add_theme_support'))
 
     // Enables post and comment RSS feed links to head
     add_theme_support('automatic-feed-links');
-
-    // Localisation Support
-    load_theme_textdomain('qstheme', THEME . '/languages');
 }
-
-/*------------------------------------*\
-	Functions
-\*------------------------------------*/
-
-// Header menu
-function header_menu() {
-	wp_nav_menu(
-		array(
-			'theme_location'  => 'header-menu',
-			'menu_class'      => 'header_menu_class',
-		)
-	);
-}
-
-// Protocol relative URLs for enqueued scripts/styles
-function html5blank_protocol_relative($url)
-{
-	if(is_admin()) return $url;
-	return str_replace(array('http:','https:'), '', $url, $c=1);
-}
-
-if ( ! function_exists( 'add_body_class' ) )
-{
-    function add_body_class( $classes ) {
-        global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
-        if( $is_lynx ) $classes[] = 'lynx';
-        elseif( $is_gecko ) $classes[] = 'gecko';
-        elseif( $is_opera ) $classes[] = 'opera';
-        elseif( $is_NS4 ) $classes[] = 'ns4';
-        elseif( $is_safari ) $classes[] = 'safari';
-        elseif( $is_chrome ) $classes[] = 'chrome';
-        elseif( $is_IE ) {
-            $classes[] = 'ie';
-            if( preg_match( '/MSIE ( [0-11]+ )( [a-zA-Z0-9.]+ )/', $_SERVER['HTTP_USER_AGENT'], $browser_version ) )
-            $classes[] = 'ie' . $browser_version[1];
-        } else $classes[] = 'unknown';
-        if( $is_iphone ) $classes[] = 'iphone';
-        
-        if ( stristr( $_SERVER['HTTP_USER_AGENT'],"mac") ) {
-                 $classes[] = 'osx';
-        } elseif ( stristr( $_SERVER['HTTP_USER_AGENT'],"linux") ) {
-                 $classes[] = 'linux';
-        } elseif ( stristr( $_SERVER['HTTP_USER_AGENT'],"windows") ) {
-                 $classes[] = 'windows';
-        }
-        
-        return $classes;
-    }
-    add_filter( 'body_class','add_body_class' );
-}
-
-// Load styles
-function qs_theme_styles()
-{
-    //wp_register_style('normalize', THEME . '/normalize.css', array(), '1.0', 'all'); wp_enqueue_style('normalize');     
-    wp_register_style('f6', FOUNDATION . '/css/foundation.css', array(), NULL, 'all'); wp_enqueue_style('f6');
-    wp_register_style('animate', THEME . '/css/animate.css', array(), '1.0', 'all'); wp_enqueue_style('animate');    
-    wp_register_style('slick', THEME . '/css/slick.css', array(), '1.0', 'all'); wp_enqueue_style('slick');   
-    wp_register_style('jquery.mCustomScrollbar.css', THEME . '/css/jquery.mCustomScrollbar.css', array(), '1.0', 'all'); wp_enqueue_style('jquery.mCustomScrollbar.css');            
-    wp_register_style('style', THEME . '/style.css', array(), '1.0', 'all'); wp_enqueue_style('style');
-    wp_register_style('responsive', THEME . '/css/responsive.css', array(), '1.0', 'all'); wp_enqueue_style('responsive');
-}
-add_action('wp_enqueue_scripts', 'qs_theme_styles'); // Add Theme Stylesheet
-
-// Load scripts	
-function qs_theme_scripts() {
-	wp_register_script( 'modern',  THEME . '/js/modern.js', array( 'jquery' ), NULL, true ); wp_enqueue_script( 'modern' );
-	wp_register_script( 'f6-input',  FOUNDATION . '/js/vendor/what-input.min.js', array( 'jquery' ), NULL, true ); wp_enqueue_script( 'f6-input' );
-    wp_register_script( 'wow', THEME . '/js/wow.min.js', array( 'jquery' ), NULL, true ); wp_enqueue_script( 'wow' );
-    wp_register_script( 'slick', THEME . '/js/slick.min.js', array( 'jquery' ), NULL, true ); wp_enqueue_script( 'slick' );
-    wp_register_script( 'scroll', THEME . '/js/smooth.scroll.js', array( 'jquery' ), NULL, true ); wp_enqueue_script( 'scroll' );
-    wp_register_script( 'mCustomScrollbar', THEME . '/js/jquery.mCustomScrollbar.js', array( 'jquery' ), NULL, true ); wp_enqueue_script( 'mCustomScrollbar' );
-	wp_register_script( 'scripts', THEME . '/js/scripts.js', array( 'jquery' ), NULL, true ); wp_enqueue_script( 'scripts' );		
-}
-add_action( 'wp_enqueue_scripts', 'qs_theme_scripts' );
-
-// Register THEME Navigation
-function register_theme_menus() {
-    register_nav_menus(array(
-        'header-menu' => __('Header Menu', 'qstheme'), // Main Navigation
-    ));
-}
-add_action('init', 'register_theme_menus');
-
-// Remove the <div> surrounding the dynamic navigation to cleanup markup
-function my_wp_nav_menu_args($args = '')
-{
-    $args['container'] = false;
-    return $args;
-}
-
-// Remove Injected classes, ID's and Page ID's from Navigation <li> items
-function my_css_attributes_filter($var)
-{
-    return is_array($var) ? array() : '';
-}
-
-// Remove invalid rel attribute values in the categorylist
-function remove_category_rel_from_category_list($thelist)
-{
-    return str_replace('rel="category tag"', 'rel="tag"', $thelist);
-}
-
-// Add page slug to body class, love this - Credit: Starkers Wordpress Theme
-function add_slug_to_body_class($classes)
-{
-    global $post;
-    if (is_home()) {
-        $key = array_search('blog', $classes);
-        if ($key > -1) {
-            unset($classes[$key]);
-        }
-    } elseif (is_page()) {
-        $classes[] = sanitize_html_class($post->post_name);
-    } elseif (is_singular()) {
-        $classes[] = sanitize_html_class($post->post_name);
-    }
-
-    return $classes;
-}
-
-// If Dynamic Sidebar Exists
-if (function_exists('register_sidebar'))
-{
-    // Define Sidebar Widget Area 1
-    register_sidebar(array(
-        'name' => __('Widget Area 1', 'qstheme'),
-        'description' => __('Description for this widget-area...', 'qstheme'),
-        'id' => 'widget-area-1',
-        'before_widget' => '<div id="%1$s" class="%2$s">',
-        'after_widget' => '</div>',
-        'before_title' => '<h3>',
-        'after_title' => '</h3>'
-    ));
-}
-
-// Remove wp_head() injected Recent Comment styles
-function my_remove_recent_comments_style()
-{
-    global $wp_widget_factory;
-    remove_action('wp_head', array(
-        $wp_widget_factory->widgets['WP_Widget_Recent_Comments'],
-        'recent_comments_style'
-    ));
-}
-
-// Pagination for paged posts, Page 1, Page 2, Page 3, with Next and Previous Links, No plugin
-function html5wp_pagination()
-{
-    global $wp_query;
-    $big = 999999999;
-    echo paginate_links(array(
-        'base' => str_replace($big, '%#%', get_pagenum_link($big)),
-        'format' => '?paged=%#%',
-        'current' => max(1, get_query_var('paged')),
-        'total' => $wp_query->max_num_pages
-    ));
-}
-
-// Custom Excerpts
-function html5wp_index($length) // Create 20 Word Callback for Index page Excerpts, call using html5wp_excerpt('html5wp_index');
-{
-    return 20;
-}
-
-// Create 40 Word Callback for Custom Post Excerpts, call using html5wp_excerpt('html5wp_custom_post');
-function html5wp_custom_post($length)
-{
-    return 40;
-}
-
-// Create the Custom Excerpts callback
-function html5wp_excerpt($length_callback = '', $more_callback = '')
-{
-    global $post;
-    if (function_exists($length_callback)) {
-        add_filter('excerpt_length', $length_callback);
-    }
-    if (function_exists($more_callback)) {
-        add_filter('excerpt_more', $more_callback);
-    }
-    $output = get_the_excerpt();
-    $output = apply_filters('wptexturize', $output);
-    $output = apply_filters('convert_chars', $output);
-    $output = '<p>' . $output . '</p>';
-    echo $output;
-}
-
-// Custom View Article link to Post
-function html5_blank_view_article($more)
-{
-    global $post;
-    return '... <a class="view-article" href="' . get_permalink($post->ID) . '">' . __('View Article', 'qstheme') . '</a>';
-}
-
-// Remove 'text/css' from our enqueued stylesheet
-function html5_style_remove($tag)
-{
-    return preg_replace('~\s+type=["\'][^"\']++["\']~', '', $tag);
-}
-
-// Remove thumbnail width and height dimensions that prevent fluid images in the_thumbnail
-function remove_thumbnail_dimensions( $html )
-{
-    $html = preg_replace('/(width|height)=\"\d*\"\s/', "", $html);
-    return $html;
-}
-
-// Custom Gravatar in Settings > Discussion
-function html5blankgravatar ($avatar_defaults)
-{
-    $myavatar = get_template_directory_uri() . '/img/gravatar.jpg';
-    $avatar_defaults[$myavatar] = "Custom Gravatar";
-    return $avatar_defaults;
-}
-
-// Threaded Comments
-function enable_threaded_comments()
-{
-    if (!is_admin()) {
-        if (is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
-            wp_enqueue_script('comment-reply');
-        }
-    }
-}
-
-// Custom Comments Callback
-function html5blankcomments($comment, $args, $depth)
-{
-	$GLOBALS['comment'] = $comment;
-	extract($args, EXTR_SKIP);
-	
-	if ( 'div' == $args['style'] ) {
-		$tag = 'div';
-		$add_below = 'comment';
-	} else {
-		$tag = 'li';
-		$add_below = 'div-comment';
-	}
-?>
-	<<?php echo $tag ?> <?php comment_class(empty( $args['has_children'] ) ? '' : 'parent') ?> id="comment-<?php comment_ID() ?>">
-	<?php if ( 'div' != $args['style'] ) : ?>
-	<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
-	<?php endif; ?>
-	<div class="comment-author vcard">
-	<?php if ($args['avatar_size'] != 0) echo get_avatar( $comment, $args['180'] ); ?>
-	<?php printf(__('<cite class="fn">%s</cite> <span class="says">says:</span>'), get_comment_author_link()) ?>
-	</div>
-<?php if ($comment->comment_approved == '0') : ?>
-	<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
-	<br />
-<?php endif; ?>
-
-	<div class="comment-meta commentmetadata"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
-		<?php
-			printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)'),'  ','' );
-		?>
-	</div>
-
-	<?php comment_text() ?>
-
-	<div class="reply">
-	<?php comment_reply_link(array_merge( $args, array('add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
-	</div>
-	<?php if ( 'div' != $args['style'] ) : ?>
-	</div>
-	<?php endif; ?>
-<?php }
-
-/*------------------------------------*\
-	Actions + Filters + ShortCodes
-\*------------------------------------*/
-
-// Add Actions
-add_action('get_header', 'enable_threaded_comments'); // Enable Threaded Comments
-add_action('widgets_init', 'my_remove_recent_comments_style'); // Remove inline Recent Comment Styles from wp_head()
-add_action('init', 'html5wp_pagination'); // Add our HTML5 Pagination
-
-// Remove Actions
-remove_action('wp_head', 'feed_links_extra', 3); // Display the links to the extra feeds such as category feeds
-remove_action('wp_head', 'feed_links', 2); // Display the links to the general feeds: Post and Comment Feed
-remove_action('wp_head', 'rsd_link'); // Display the link to the Really Simple Discovery service endpoint, EditURI link
-remove_action('wp_head', 'wlwmanifest_link'); // Display the link to the Windows Live Writer manifest file.
-remove_action('wp_head', 'index_rel_link'); // Index link
-remove_action('wp_head', 'parent_post_rel_link', 10, 0); // Prev link
-remove_action('wp_head', 'start_post_rel_link', 10, 0); // Start link
-remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0); // Display relational links for the posts adjacent to the current post.
-remove_action('wp_head', 'wp_generator'); // Display the XHTML generator that is generated on the wp_head hook, WP version
-remove_action('wp_head', 'start_post_rel_link', 10, 0);
-remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
-remove_action('wp_head', 'rel_canonical');
-remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
-
-// Add Filters
-add_filter('avatar_defaults', 'html5blankgravatar'); // Custom Gravatar in Settings > Discussion
-add_filter('body_class', 'add_slug_to_body_class'); // Add slug to body class (Starkers build)
-add_filter('widget_text', 'do_shortcode'); // Allow shortcodes in Dynamic Sidebar
-add_filter('widget_text', 'shortcode_unautop'); // Remove <p> tags in Dynamic Sidebars (better!)
-add_filter('wp_nav_menu_args', 'my_wp_nav_menu_args'); // Remove surrounding <div> from WP Navigation
-add_filter('the_category', 'remove_category_rel_from_category_list'); // Remove invalid rel attribute
-add_filter('the_excerpt', 'shortcode_unautop'); // Remove auto <p> tags in Excerpt (Manual Excerpts only)
-add_filter('the_excerpt', 'do_shortcode'); // Allows Shortcodes to be executed in Excerpt (Manual Excerpts only)
-add_filter('excerpt_more', 'html5_blank_view_article'); // Add 'View Article' button instead of [...] for Excerpts
-add_filter('style_loader_tag', 'html5_style_remove'); // Remove 'text/css' from enqueued stylesheet
-add_filter('post_thumbnail_html', 'remove_thumbnail_dimensions', 10); // Remove width and height dynamic attributes to thumbnails
-add_filter('image_send_to_editor', 'remove_thumbnail_dimensions', 10); // Remove width and height dynamic attributes to post images
-
-// Remove Filters
-remove_filter('the_excerpt', 'wpautop'); // Remove <p> tags from Excerpt altogether
