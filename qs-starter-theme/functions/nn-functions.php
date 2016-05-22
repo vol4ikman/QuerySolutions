a4c5d4f91eaeb077a259de14859ded657984a752
nivnoiman <niv945@gmail.com>
2016-05-22 16:43:26 +0300

    New Time ago function added
    New Time ago function added
    for example :
    from -> 2016-05-022 00:11:31
    to - > 4 months ago

--- qs-starter-theme/functions/nn-functions.php
+++ qs-starter-theme/functions/nn-functions.php
@@ -298,3 +298,39 @@ function getUserType(){
 
     return $return;
 }
+
+
+/**
+ * [getTimeAgo description]
+ * @method getTimeAgo
+ * @param  [type] $datetime [ any date and time format ]
+ * @param  [type] $full [ output type - if true the result will be all the time stamp ]
+ * @return [type] [ return the time ago ]
+ */
+function getTimeAgo( $datetime, $full = false ) {
+    $now  = new DateTime;
+    $ago  = new DateTime( $datetime );
+    $diff = $now->diff( $ago );
+
+    $diff->w  = floor( $diff->d / 7 );
+    $diff->d -= $diff->w * 7;
+
+    $string = array(
+        'y' => 'year',
+        'm' => 'month',
+        'w' => 'week',
+        'd' => 'day',
+        'h' => 'hour',
+        'i' => 'minute',
+        's' => 'second',
+    );
+    foreach ( $string as $k => &$v ) {
+        if ( $diff->$k ) {
+            $v = $diff->$k . ' ' . $v . ( $diff->$k > 1 ? 's' : '' );
+        } else
+            unset( $string[ $k ] );
+    }
+
+    if ( !$full ) $string = array_slice( $string, 0, 1 );
+    return $string ? implode( ', ', $string ) . ' ago' : 'just now';
+}
