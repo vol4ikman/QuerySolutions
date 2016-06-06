4883b89999e594087be0ba2ccef7fe8d5e3e1a2a
Yuval Tsabar <yuvalsabar@gmail.com>
2016-06-06 14:31:03 +0300

    Update view-anchor.php

--- VC Example/view/view-anchor.php
+++ VC Example/view/view-anchor.php
@@ -1,3 +1,3 @@
 <?php get_query_var('data', $data);?>
 
-<a name="<?php echo $data['title'];?>" class="anchor_section"></a>
+<a name="<?php echo $data['title'];?>" class="anchor_section <?php echo $data['css_class'];?>"></a>
