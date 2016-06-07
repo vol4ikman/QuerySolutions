e644b39c8e007532f774f3844f16a7f67390c7fb
Yuval Tsabar <yuvalsabar@gmail.com>
2016-06-07 14:52:36 +0300

    Create get_posts_by_first_letter.php

---
+++ Functions/get_posts_by_first_letter.php
@@ -0,0 +1,6 @@
+function get_posts_by_first_letter($post_type, $first_letter) {
+	global $wpdb, $post; 
+    $sql = "SELECT * FROM $wpdb->posts WHERE post_title LIKE '".$first_letter."%' AND post_type = '".$post_type."' AND post_status = 'publish'; ";
+	$results = $wpdb->get_results($sql);
+	return $results;
+}
