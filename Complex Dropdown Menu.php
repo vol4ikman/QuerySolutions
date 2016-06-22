6623b196bede716b62d626149954a5a8a85c1c57
Yuval Tsabar <yuvalsabar@gmail.com>
2016-06-22 16:56:03 +0300

    Update Complex Dropdown Menu.php

--- Complex Dropdown Menu.php
+++ Complex Dropdown Menu.php
@@ -66,3 +66,18 @@ function my_edit_dropdown_columns( $columns ) {
 
     return $columns;
 }
+
+// shortcode
+
+function submenu_shortcode($atts) {
+	$atts = shortcode_atts( array(
+        'id' => ''
+    ), $atts );
+
+    set_query_var('atts' , $atts);
+
+    ob_start();
+    get_template_part('inc/shortcode','submenu');
+    return ob_get_clean();
+}
+add_shortcode( 'submenu', 'submenu_shortcode' );
