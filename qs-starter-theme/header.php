2f89832ad673a84b9c911ac85f7cb6f1d72bb492
nivnoiman <niv945@gmail.com>
2016-07-03 14:43:50 +0300

    Remove SVG Logo
    Remove SVG Logo

--- qs-starter-theme/header.php
+++ qs-starter-theme/header.php
@@ -31,8 +31,6 @@
 					<!-- logo -->
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
 						</a>
 					</div>
 					<!-- /logo -->
