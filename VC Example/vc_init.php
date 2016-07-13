ae1de0472476207e096451e5696cc6f19e3fe5f3
Yuval Tsabar <yuvalsabar@gmail.com>
2016-07-13 12:11:42 +0300

    Update vc_init.php

--- VC Example/vc_init.php
+++ VC Example/vc_init.php
@@ -1,4 +1,5 @@
-define ('VC_MAP', 'admin/vc/map/map');
-define ('VC_VIEW', '/admin/vc/view/view');
+<?php 
 
-get_template_part( VC_MAP, 'anchor' );
+define ('VC_SCRIPT', get_template_directory_uri().'/vc/js/vc_script.js');
+define ('VC_MAP', '/vc/map/map');
+define ('VC_VIEW', '/vc/view/view');
