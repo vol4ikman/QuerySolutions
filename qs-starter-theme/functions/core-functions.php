e78965fc9a45c95162a81b1bfd61a7861b59a136
Alex Volkov <vol4ikman@gmail.com>
2016-11-28 17:04:35 +0200

    Update core-functions.php

--- qs-starter-theme/functions/core-functions.php
+++ qs-starter-theme/functions/core-functions.php
@@ -23,23 +23,6 @@ function remove_category_rel_from_category_list($thelist){
     return str_replace('rel="category tag"', 'rel="tag"', $thelist);
 }
 
-// Add page slug to body class, love this - Credit: Starkers Wordpress Theme
-function add_slug_to_body_class($classes){
-    global $post;
-    if (is_home()) {
-        $key = array_search('blog', $classes);
-        if ($key > -1) {
-            unset($classes[$key]);
-        }
-    } elseif (is_page()) {
-        $classes[] = sanitize_html_class($post->post_name);
-    } elseif (is_singular()) {
-        $classes[] = sanitize_html_class($post->post_name);
-    }
-
-    return $classes;
-}
-
 // If Dynamic Sidebar Exists
 if (function_exists('register_sidebar')){
     // Define Sidebar Widget Area 1
